bdabe9324e8629b5befdb786e62cda34291ce647
mnsnowball <35165513+mnsnowball@users.noreply.github.com>
2021-03-28 17:38:35 +0100

    Added rigged character, baked lighting
    Added level select screen and animations for elements

---
+++ TimeRewind/Assets/Scripts/FX/ObjectHover.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class ObjectHover : MonoBehaviour
+{
+    Animator myAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        //Debug.Log("Clicked");
+        myAnim.SetBool("Idle", false);
+        myAnim.SetBool("Clicked", true);
+    }
+
+    private void OnMouseExit()
+    {
+        myAnim.SetBool("Clicked", false);
+        myAnim.SetBool("Idle", true);
+    }
+}
---
+++ TimeRewind/Assets/Scripts/LevelSelection.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelection : MonoBehaviour
+{
+    // this class will go on the level select screen books to turn them into buttons
+
+    bool canClickMe = false;
+    bool isLoading = false;
+    public Outline outline;
+    public LevelLoader loader;
+    public int levelIndex = 0;
+    Animator anim;
+    public ParticleSystem particles;
+    ParticleSystem.EmissionModule emission;
+    private void OnMouseEnter()
+    {
+        if (!isLoading)
+        {
+            canClickMe = true;
+            outline.enabled = true;
+            anim.SetBool("isHovering", true);
+            // enable particles
+            emission.enabled = true;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (!isLoading)
+        {
+            canClickMe = false;
+            outline.enabled = false;
+            anim.SetBool("isHovering", false);
+            // disable particles
+            emission.enabled = false;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (!isLoading)
+        {
+            loader.LoadALevel(levelIndex);
+            isLoading = true;
+        }
+    }
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        emission = particles.emission;
+        canClickMe = false;
+        outline.enabled = false;
+        anim.SetBool("isHovering", false);
+        // disable particles
+        emission.enabled = false;
+    }
+
+    private void Update()
+    {
+        
+    }
+}
